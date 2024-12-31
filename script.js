00d69c8254a7cf218a719369cc0e0f6691c775b0
Rafael Mestre <rafaelmestredosanjos@gmail.com>
2024-12-31 15:00:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function toggleMenu() {
     const menu = document.getElementById('menu');
-    menu.classList.toggle('show');
-  }
+    menu.classList.toggle('show'); // Adiciona ou remove a classe "show" no menu
+}
